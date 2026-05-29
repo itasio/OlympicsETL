ea2b7d3ac7d54e9f4a6e3243d3ac4f9827dcdcae
itasio <john.7tasi@gmail.com>
2026-05-29 19:06:54 +0300

    add docker-compose.yml for postgres connectivity and queries in db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from db import engine
+from sqlalchemy import inspect
 
 
 def clean_my_dataset(df_to_clean: pd.DataFrame) -> pd.DataFrame:
@@ -16,19 +17,71 @@ def clean_my_dataset(df_to_clean: pd.DataFrame) -> pd.DataFrame:
     return df_to_clean
 
 
-def send_df_to_db(df_to_load):
-    # create DB connection
-    engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
 
-    # load data into table
-    df_to_load.to_sql(
-        "athlete_events",
-        engine,
-        if_exists="replace",
-        index=False
-    )
+def query_the_df(df):
+    # The 3 oldest athletes to ever have won a medal
+    oldest_medalists = df[df["won_medal"] == True].sort_values("age", ascending=False)
 
-    return engine
+    print("\nOldest Medalists\n")
+    print(oldest_medalists[["name", "age", "team"]].head(3))
+
+    # Number of unique (different) athletes competed over time by country
+    athletes_teams = df[["id", "team"]].drop_duplicates()
+    ath_by_country = athletes_teams.groupby("team")["id"].count().sort_values(ascending=False)
+
+    print("\nUnique athletes by country all those years sorted\n")
+    print(ath_by_country.head(5))
+
+    # The number of medals won by country sorted
+    medals_per_country = (df[df["medal"].notna()]
+                          .groupby("team")["medal"].count()
+                          .sort_values(ascending=False))
+
+    print("\nMedals per country sorted\n")
+    print(medals_per_country.head(5))
+
+
+def query_the_db(tbl_name):
+
+    # The athletes with the most participations in olympics
+
+    query = f"""
+            SELECT name, COUNT(*) as participations
+            FROM {tbl_name}
+            GROUP BY name
+            ORDER BY participations DESC
+            limit 5
+            """
+    result = pd.read_sql(query, engine)
+
+    print(f"\nQuery {tbl_name} to show the athletes with the most participations\n")
+    print(result)
+
+
+    # Who is the athlete that has won the most medals
+
+    query = f"""
+             SELECT name, COUNT(medal) as medals_won
+             FROM {tbl_name}
+             WHERE medal IS NOT NULL
+             GROUP BY name
+             ORDER BY medals_won DESC
+             limit 5
+            """
+    result = pd.read_sql(query, engine)
+
+    print(f"\nQuery {tbl_name} to show the top athletes that have won the most medals\n")
+    print(result)
+
+
+def load_df_to_db(df, tbl_name):
+    inspector = inspect(engine)
+
+    if inspector.has_table(tbl_name):
+        print(f"\nTable '{tbl_name}' already exists.\n")
+    else:
+        df.to_sql(tbl_name, engine, index=False)
+        print(f"\nTable '{tbl_name}' created.\n")
 
 
 if __name__ == '__main__':
@@ -38,59 +91,18 @@ if __name__ == '__main__':
     # Do some cleaning
     df = clean_my_dataset(df)
 
+    print("Cleaned Dataset\n")
+    print(df.head(5))
 
-    print("Cleaned Dataset")
-    print(df.head(20))
-
-    # The 3 oldest athletes to ever have won a medal
-    df.query("won_medal == True")
-    print(df.query("won_medal == True").head(20))
-    oldest_medalists = df[df["won_medal"] == True].sort_values("age", ascending=False)
-
-    print("Oldest Medalists")
-    print(oldest_medalists[["name", "age", "team"]].head(3))
+    # Make some demo queries to dataframe
+    query_the_df(df)
 
 
-    # Number of unique (different) athletes competed over time by country
-    athletes_teams = df[["id", "team"]].drop_duplicates()
-    ath_by_country = athletes_teams.groupby("team")["id"].count()
+    # load df to db if it does not exist
+    tbl_name = "athlete_events"
+    load_df_to_db(df, tbl_name)
 
-    print("Unique athletes by country all those years")
-    print(ath_by_country.head(20))
-
-    # The number of medals won by country sorted
-    medals_per_country = (df[df["medal"].notna()]
-                          .groupby("team")["medal"].count()
-                          .sort_values(ascending=False))
-
-    print("Medals per country sorted")
-    print(medals_per_country.head(20))
-
-    # load df to db
-    the_engine = send_df_to_db(df)
 
     # Query the db
-
-    # query = """
-    #         SELECT name, team
-    #         FROM athlete_events
-    #         limit 10
-    #         """
-    # result = pd.read_sql(query, the_engine)
-    # print(result)
-
-
-    # Who is the athlete that has won the most medals
-
-    # query = """
-    #          SELECT name, COUNT(medal) as medals_won
-    #          FROM athletes_events
-    #          WHERE medal IS NOT NULL
-    #          GROUP BY name
-    #          ORDER BY medals_won DESC
-    #          limit 1
-    #         """
-    # result = pd.read_sql(query, the_engine)
-    # print(result)
-
+    query_the_db(tbl_name)
 
