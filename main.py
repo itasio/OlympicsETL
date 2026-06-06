d99e44049063a213c418a483db099cdbf30d289d
itasio <john.7tasi@gmail.com>
2026-06-06 12:00:06 +0300

    add .env file, enrich cleaning of dataframes

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ from db import engine
 from sqlalchemy import inspect
 
 
-def clean_my_dataset(df_to_clean: pd.DataFrame) -> pd.DataFrame:
-    df_to_clean.columns = df_to_clean.columns.str.strip().str.lower().str.replace(" ", "_")
-    df_to_clean = df_to_clean.drop_duplicates()
+def clean_athlete_events(ath_ev_df: pd.DataFrame, country_def_df: pd.DataFrame) -> pd.DataFrame:
+    ath_ev_df.columns = ath_ev_df.columns.str.strip().str.lower().str.replace(" ", "_")
+    ath_ev_df = ath_ev_df.drop_duplicates()
 
-    df_to_clean = df_to_clean[df_to_clean["age"] >= 0]
-    df_to_clean = df_to_clean[df_to_clean["height"] >= 0]
-    df_to_clean = df_to_clean[df_to_clean["weight"] >= 0]
-    df_to_clean = df_to_clean[df_to_clean["year"] >= 1896]
+    ath_ev_df = ath_ev_df[ath_ev_df["age"] >= 0]
+    ath_ev_df = ath_ev_df[ath_ev_df["height"] >= 0]
+    ath_ev_df = ath_ev_df[ath_ev_df["weight"] >= 0]
+    ath_ev_df = ath_ev_df[ath_ev_df["year"] >= 1896]
 
-    df_to_clean["bmi"] = (df_to_clean["weight"] / ((df_to_clean["height"] / 100) ** 2)).round(3)
-    df_to_clean["won_medal"] = df_to_clean["medal"].notna()
-    return df_to_clean
+    ath_ev_df["bmi"] = (ath_ev_df["weight"] / ((ath_ev_df["height"] / 100) ** 2)).round(3)
+    ath_ev_df["won_medal"] = ath_ev_df["medal"].notna()
+
+    # Keep only the rows in which value of column NOC exists in country definitions
+
+    noc_column = ath_ev_df["noc"]
+    mask = noc_column.isin(country_def_df["noc"])
+    print(f"\nThe rows in athlete_events.csv containing NOC values not included in\n"
+          f"country_definitions.csv and hence omitted are: {(~mask).sum()} \n")
+
+    ath_ev_df = ath_ev_df[mask]
+    return ath_ev_df
+
+def clean_country_definitions(df: pd.DataFrame) -> pd.DataFrame:
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+    df = df.drop_duplicates()
+
+    # Alter the NOC of Singapore. It is SGP instead of SIN since 2016
+    mask = (df["noc"] == "SIN") & (df["region"] == "Singapore")
+    print(f"Rows changed in country definitions for Singapore naming: {mask.sum()} \n")
+    df.loc[mask, "noc"] = "SGP"
+
+    return df
 
 
 
@@ -74,35 +94,49 @@ def query_the_db(tbl_name):
     print(result)
 
 
-def load_df_to_db(df, tbl_name):
+
+def load_df_to_db(df, tbl_name:str, replace_if_exists:bool=False):
+    """
+
+    :param df: the df to load
+    :param tbl_name: the name of the table to be created in database
+    :param replace_if_exists: if table exists in db and variable is set to true, table will be replaced, if set to false table will not be replaced. If table does not exist in db, it will be created.
+    """
     inspector = inspect(engine)
 
     if inspector.has_table(tbl_name):
         print(f"\nTable '{tbl_name}' already exists.\n")
+        print(f"Replace will is set to {replace_if_exists}\n")
+        if replace_if_exists:
+            df.to_sql(tbl_name, engine, index=False, if_exists="replace")
     else:
+        # Table does not exist in db, create it
         df.to_sql(tbl_name, engine, index=False)
         print(f"\nTable '{tbl_name}' created.\n")
 
-
 if __name__ == '__main__':
-    # Read CSV
-    df = pd.read_csv("data/athlete_events.csv")
+    # Read CSVs
+    ath_events_df = pd.read_csv("data/athlete_events.csv")
+    country_definitions_df = pd.read_csv("data/country_definitions.csv")
 
     # Do some cleaning
-    df = clean_my_dataset(df)
+    country_definitions_df = clean_country_definitions(country_definitions_df)
+    ath_events_df = clean_athlete_events(ath_events_df, country_definitions_df)
 
     print("Cleaned Dataset\n")
-    print(df.head(5))
+    print(ath_events_df.head(5))
 
     # Make some demo queries to dataframe
-    query_the_df(df)
+    query_the_df(ath_events_df)
 
 
-    # load df to db if it does not exist
-    tbl_name = "athlete_events"
-    load_df_to_db(df, tbl_name)
+    # load dfs to db
+    tbl_events = "athlete_events"
+    load_df_to_db(ath_events_df, tbl_events)
 
+    tbl_countries = "country_definitions"
+    load_df_to_db(country_definitions_df, tbl_countries)
 
     # Query the db
-    query_the_db(tbl_name)
+    query_the_db(tbl_events)
 
